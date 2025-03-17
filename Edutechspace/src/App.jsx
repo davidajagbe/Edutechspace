8c6b98c2d64ec17ca5082129b1d81ba97b094c7f
davidajagbe <davidajagbe2004@gmail.com>
2025-03-17 14:53:46 +0100

    fixed loading page

--- Edutechspace/src/App.jsx
+++ Edutechspace/src/App.jsx
@@ -1,5 +1,7 @@
-import { createBrowserRouter, createRoutesFromElements, RouterProvider,Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import MainLayout from './layout/MainLayout';
+import LoadingPage from './pages/LoadingPage';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/signup';
@@ -11,25 +13,35 @@ import NotFound from './component/NotFoundPage';
 import CertificateExam from './pages/CertificationExam';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Show loading screen for 3 seconds
+  }, []);
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<MainLayout/>}>
-        <Route index element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/course' element={<Course/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/coursedatabase' element={<CourseDatabase/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/certification-exam' element={<CertificateExam/>}/>
-        <Route path='*' element={<NotFound/>}/>
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/course' element={<Course />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/coursedatabase' element={<CourseDatabase />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/certification-exam' element={<CertificateExam />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     )
-  )
+  );
 
-  return(
-    <RouterProvider router={router}/>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
