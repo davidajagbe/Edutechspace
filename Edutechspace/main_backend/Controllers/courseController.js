6f4117e88d882320068c2db84e36bb24053fcae5
Danny944 <fadehandaniel2006@gmail.com>
2025-05-10 13:48:23 +0100

    feat(course): get courses implemented

--- Edutechspace/main_backend/Controllers/courseController.js
+++ Edutechspace/main_backend/Controllers/courseController.js
@@ -2,15 +2,13 @@ import supabase from '../config/supabase.js';
 
 
 export const getCourses = async (req, res) => {
-  console.log('getCourses: Fetching courses');
   try {
-    const { data, error } = await supabase.from('courses').select('*');
+    const { data, error } = await supabase.from('courses').select('name');
     if (error) {
-      console.error('getCourses: Supabase error:', error.message);
       throw new Error(error.message);
     }
+    if(data.length == 0) return res.json({message: "no courses available"})
 
-    console.log('getCourses: Data:', data);
     res.json(data);
   } catch (err) {
     console.error('getCourses: Error:', err.message);
